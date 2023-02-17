d5f78246d7527a4c5c6cacaf7fb3bc3402b33ce8
Antony_wes <102688774+AntonyWes@users.noreply.github.com>
2023-02-17 17:36:03 +0300

    Add files via upload

---
+++ ПроверкаCodeforces.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	string codeforces = "codeforces";
+	cin >> num;
+	string let;
+	bool mass[26];
+	if (1 <= num && num <= 26) {
+		for (int i = 0; i < num; i++)
+		{
+			cin >> let;
+			if (codeforces.find(let) != string::npos)
+			{
+				mass[i] = true;
+			}
+			else mass[i] = false;
+		}
+	}
+	else cout << "Number isn`t between 1 to 26" << endl;
+	for (int i = 0; i < num; i++) 
+	{
+		if (mass[i])
+		{
+			cout << "YES" << endl;
+		}
+		else cout << "NO" << endl;
+	}
+
+}
